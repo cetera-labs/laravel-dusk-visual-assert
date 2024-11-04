766576c92208d26a54ad82895327d5d6b8bee7ad
Roman Romanov <nicodim@mail.ru>
2024-11-04 13:28:13 +0500

    Update visual-assert.php

--- config/visual-assert.php
+++ config/visual-assert.php
@@ -1,16 +1,8 @@
 <?php
 
 return [
-
-    // Keep the screenshots the same size for better comparison
-    'screenshot_width' => 1920,
-    'screenshot_height' => 1080,
-
-
     // For more info on how images are compared see
     // https://www.php.net/manual/en/imagick.compareimages.php
     'default_threshold' => 0.0001,
     'default_metric' => \Imagick::METRIC_MEANSQUAREERROR,
-
-    'skip_if_different_window_size' => false,
 ];
