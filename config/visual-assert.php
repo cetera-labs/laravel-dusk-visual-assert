a76557b62c893120b636c929f3465d5be7240466
Roman Romanov <nicodim@mail.ru>
2024-11-05 20:51:14 +0500

    Update visual-assert.php

--- config/visual-assert.php
+++ config/visual-assert.php
@@ -4,5 +4,5 @@ return [
     // For more info on how images are compared see
     // https://www.php.net/manual/en/imagick.compareimages.php
     'default_threshold' => 0.0001,
-    'default_metric' => \Imagick::METRIC_MEANSQUAREERROR,
+    'default_metric' => 4, // \Imagick::METRIC_MEANSQUAREERROR
 ];
